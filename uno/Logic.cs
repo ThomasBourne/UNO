0ccb6b51e7a96fd178cc578b682458b282155e01
Thomas Bourne <69239859+ThomasBourne@users.noreply.github.com>
2021-02-01 13:34:12 +0000

    Add files via upload

---
+++ uno/Logic.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace uno
+{
+    class Logic
+    {
+        public void ShowDeck(List<string[]> deck, string name, string[] pile) //Func that Prints the players deck to the console
+        {
+            Console.WriteLine($"{name}'s turn:\nCards below:");
+            string output;
+            for (int i = 0; i < deck.Count; i++)
+            {
+                Console.Write($"{i + 1}. ");
+                if (deck[i][1] == "Yellow")
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                if (deck[i][1] == "Blue")
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                if (deck[i][1] == "Red")
+                    Console.ForegroundColor = ConsoleColor.Red;
+                if (deck[i][1] == "Green")
+                    Console.ForegroundColor = ConsoleColor.Green;
+                output = $"{deck[i][0]}, {deck[i][1]}\n";
+                Console.Write(output);
+                Console.ResetColor();
+            }
+            Console.WriteLine($"Card on the top of the pile is {pile[0]}, {pile[1]}");
+            Console.WriteLine("");
+        }
+        public List<string[]> ChangeCard(List<string[]> deck, List<string[]> card, int pos) //Func may not be implemented or fully built
+        {
+            //deck[pos] = card;
+            return deck;
+        }
+        public string[] CollectNames(int playerCount) //func that collects players names
+        {
+            string[] names = new string[playerCount];
+            for (int i = 0; i < playerCount; i++)
+            {
+                Console.Write($"Enter Name for player {i + 1}\n>>> ");
+                string name = Console.ReadLine();
+                names[i] = name;
+            }
+            //Done here just to clean up Run.cs
+            Console.WriteLine($"Push Enter to start {names[0]}'s turn");
+            Console.ReadLine();
+            return names;
+        }
+        public List<string[]> Plus(List<string[]> deck, string card) //adds cards to a players deck
+        {
+            Deck deckGen = new Deck();
+            int repeat;
+            if(card == "+2")
+            {
+                repeat = 2;
+            }
+            else
+            {
+                repeat = 4;
+            }
+            for (int i = 0; i < repeat; i++)
+            {
+                deck.Add(deckGen.GenerateCard());
+            }
+            return deck;
+        }
+        public (string[], List<string[]>) Play(string[] oldPile, List<string[]> playersDeck) //The highest level of card logic
+        /* BUGS
+         * when the card num gets low enough the play cannot pick up cards
+         */
+        {
+            //creates object of Deck class
+            Deck deck = new Deck();
+
+            //valid indexs that of the deck that match the pile card
+            List<int> validIndexes = GetValidIndexes(oldPile, playersDeck);
+            //the input value
+            int index = GetRangeInput(validIndexes);
+            if (index == Store.NumForPickup)
+            {
+                playersDeck.Add(deck.GenerateCard());
+                return (oldPile, playersDeck);
+            }
+
+            string[] newPile = playersDeck[index];
+            playersDeck.RemoveAt(index);
+            return (newPile, playersDeck);
+        }
+        private int GetNumericInput()
+        {
+            Console.Write("Enter the index of the card you want to play (leave blank to pickup)\n>>> ");
+            string input = Console.ReadLine();
+            while (!int.TryParse(input, out int asd))
+            {
+                if (input == "" || input == "\n")
+                    return Store.NumForPickup;
+                Console.Write("Value not valid\nEnter the index of the card you want to play (leave blank to pickup)\n>>> ");
+                input = Console.ReadLine();
+            }
+            int numIndex = int.Parse(input);
+            return numIndex;
+        }
+        private int GetRangeInput(List<int> validIndexes)
+        {
+            
+            int index = GetNumericInput();
+            //bool valid = false;
+            while (true)
+            {
+                for (int i = 0; i < validIndexes.Count; i++)
+                {
+                    if (index -1 == validIndexes[i] || index == Store.NumForPickup)
+                    {
+                        //valid = !valid;
+                        if (index != Store.NumForPickup)
+                            return index - 1;
+                        return index;
+                    }
+                }
+                index = GetNumericInput();
+            }
+        }
+        private List<int> GetValidIndexes(string[] pile, List<string[]> deck)
+        {
+            List<int> validIndexes = new List<int>();
+            for (int i = 0; i < deck.Count; i++)
+            {
+                if (pile[0] == deck[i][0] || pile[1] == deck[i][1])
+                {
+                    validIndexes.Add(i);
+                }
+            }
+            return validIndexes;
+        }
+    }
+}
---
+++ uno/Run.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace uno
+{
+    class Run
+    {
+        static void Main(string[] args)
+        {
+            //creating the class objects
+            Deck deck = new Deck();
+            Logic logic = new Logic();
+            Store store = new Store();
+
+            //gets the number of players
+            int numPlayers = NumPlayers();
+            Console.WriteLine(numPlayers);
+
+            //gives each player a name
+            string[] names = logic.CollectNames(numPlayers);
+
+            //just to let them hand over the device to start players 0's turn
+            System.Threading.Thread.Sleep(2000);
+            Console.Clear();
+
+            //creates the starting played pile
+            string[] pile = new string[1];
+            pile = deck.GenerateCard();
+
+
+            //generates player objects and their randomly generated cards
+            Player player1 = new Player();
+            player1.getValues(deck.GenerateStartupDeck());
+            Player player2 = new Player();
+            player2.getValues(deck.GenerateStartupDeck());
+            Player player3 = new Player();
+            player3.getValues(deck.GenerateStartupDeck());
+            Player player4 = new Player();
+            player4.getValues(deck.GenerateStartupDeck());
+            Player player5 = new Player();
+            player5.getValues(deck.GenerateStartupDeck());
+            Player player6 = new Player();
+            player6.getValues(deck.GenerateStartupDeck());
+            Player player7 = new Player();
+            player7.getValues(deck.GenerateStartupDeck());
+
+            //puts all of the players into a list
+            List<Player> players = new List<Player>();
+
+            //the players are added to the players here (could have used an array but List is easier to modify)
+            for (int i = 0; i < numPlayers; i++)
+            {
+                if (i == 0)
+                {
+                    players.Add(player1);
+                    players.Add(player2);
+                }
+                if (i == 2)
+                    players.Add(player3);
+                if (i == 3)
+                    players.Add(player4);
+                if (i == 4)
+                    players.Add(player5);
+                if (i == 5)
+                    players.Add(player6);
+                if (i == 6)
+                    players.Add(player7);
+
+            }
+            //where the game played
+            /* To Fix/Add
+             * what happens when a player reaches 0 cards
+             * any card can be chosen
+             * preformance fixes/optimisation
+             */
+            while (true)
+            {
+                for (int i = 0; i < numPlayers; i++)
+                {
+                    logic.ShowDeck(players[i].Deck(), names[i], pile);
+                    List<string[]> tmp = new List<string[]>();
+                    (pile, tmp) = logic.Play(pile, players[i].Deck());
+                    players[i].getValues(tmp);
+                    Console.Clear();
+                    System.Threading.Thread.Sleep(5000);
+                }
+            }
+        }
+        public static int NumPlayers() //Func that gets number of players
+        {
+            //generate variables to be used
+            int numPlayers;
+            string strNumPlayers;
+            //gets input
+            Console.Write("Enter the number of players (2-7)\n>>> ");
+            strNumPlayers = Console.ReadLine();
+            //checks if it is numeric AND in the range of 2 to 7
+            while (strNumPlayers.Any(c => c < '2' || c > '7'))
+            {
+                Console.Write("Please enter a number between 2 and 7\n>>> ");
+                strNumPlayers = Console.ReadLine();
+                Console.Clear();
+            }
+            //converts to int and returns
+            numPlayers = Convert.ToInt32(strNumPlayers);
+            return numPlayers;
+        }
+    }
+}
